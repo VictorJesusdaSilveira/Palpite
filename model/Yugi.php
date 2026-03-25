78964a65af25854ee1bfa41cd2b8d946151855ca
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-25 16:52:51 -0300

    Create Yugi.php

---
+++ model/Yugi.php
@@ -0,0 +1,72 @@
+<?php
+
+class Yugi{
+    private string $nome;
+    private string $link;
+    private string $dica;
+
+    public function __construct(string $nome, string $link, string $dica){
+        $this->nome = $nome;
+        $this->link = $link;
+        $this->dica = $dica;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of link
+     */
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set the value of link
+     */
+    public function setLink(string $link): self
+    {
+        $this->link = $link;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of dica
+     */
+    public function getDica(): string
+    {
+        return $this->dica;
+    }
+
+    /**
+     * Set the value of dica
+     */
+    public function setDica(string $dica): self
+    {
+        $this->dica = $dica;
+
+        return $this;
+    }
+}
+
+
+
+
+?>
